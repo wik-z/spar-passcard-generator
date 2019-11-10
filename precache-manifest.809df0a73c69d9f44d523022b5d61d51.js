cd3b82817d1c9a9c7f7aee25999b79f072635f92
Wiktor Zawierucha <26853175+wik-z@users.noreply.github.com>
2019-11-10 02:14:16 +0000

    Updates

---
+++ precache-manifest.809df0a73c69d9f44d523022b5d61d51.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921b1b92b7394b99bb71d6653be762d7",
+    "url": "/spar-passcard-generator/index.html"
+  },
+  {
+    "revision": "fcb438b881d2af73c038",
+    "url": "/spar-passcard-generator/static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "2423c476105781e0c489",
+    "url": "/spar-passcard-generator/static/css/main.d3ed2bbc.chunk.css"
+  },
+  {
+    "revision": "fcb438b881d2af73c038",
+    "url": "/spar-passcard-generator/static/js/2.0bbf45c5.chunk.js"
+  },
+  {
+    "revision": "2423c476105781e0c489",
+    "url": "/spar-passcard-generator/static/js/main.97b5f332.chunk.js"
+  },
+  {
+    "revision": "34d14edb99604180f8ed",
+    "url": "/spar-passcard-generator/static/js/runtime-main.d69e6e26.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/spar-passcard-generator/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "fbf87001105c3d03f6a9976b44f8b683",
+    "url": "/spar-passcard-generator/static/media/logo.fbf87001.png"
+  },
+  {
+    "revision": "75fc9417dafd4e4255165fe27f3f90af",
+    "url": "/spar-passcard-generator/static/media/saf-logo.75fc9417.png"
+  },
+  {
+    "revision": "e8ef4d9bb4f3c7e6396bc8a5c3bad63c",
+    "url": "/spar-passcard-generator/static/media/sso-logo.e8ef4d9b.png"
+  },
+  {
+    "revision": "93da7d1a6a709ffcb049bde4017d5058",
+    "url": "/spar-passcard-generator/static/media/stf-logo.93da7d1a.png"
+  }
+]);
